fda50a31d99805eea92acfc29e5086a37e00235d
Filipi <you@example.com>
2015-03-06 20:57:36 -0300

    Aplicacao das funcoes

--- variaveis-compostas.c
+++ variaveis-compostas.c
@@ -10,10 +10,16 @@ struct conta {
 };
 
 void exibeConta (struct conta, char *);
-oid leConta (struct conta *, char *);
+void leConta (struct conta *, char *);
 int main()
 {
     struct conta conta1, conta2;
+    printf("\nEndereco de conta 1: %p", &conta1);
+    printf("\nEndereco de conta 2: %p", &conta2);
+    printf("\nEndereco do numero da conta 1: %p", &conta1.numero);
+    printf("\nEndereco do saldo da conta 1: %p", &conta1.saldo);
+    printf("\nEndereco do numero da conta 2: %p", &conta2.numero);
+    printf("\nEndereco do saldo da conta 2: %p", &conta2.saldo);
     conta1.numero = 10;
     conta1.saldo =100;
     leConta(&conta2, "Digite os dados para a conta2: ");
@@ -33,7 +39,7 @@ void leConta(struct conta *c, char * msg)
 {
     printf("\n%s", msg);
     printf("Digite o numero da conta: ");
-    scanf("%d", c->numero);
+    scanf("%d", &c->numero);
     printf("Digite o saldo da conta: ");
-    scanf("%.2f", c->saldo);
+    scanf("%.2f", &c->saldo);
 }
