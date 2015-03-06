78b07419113ecb322eef0ec3252a2c725de94772
Filipi <you@example.com>
2015-03-06 20:05:34 -0300

    Refatorando para funcoes e ponteiros

--- variaveis-compostas.c
+++ variaveis-compostas.c
@@ -10,16 +10,13 @@ struct conta {
 };
 
 void exibeConta (struct conta, char *);
-
+oid leConta (struct conta *, char *);
 int main()
 {
     struct conta conta1, conta2;
     conta1.numero = 10;
     conta1.saldo =100;
-    printf("digite o numero da conta: ");
-    scanf("%d", &conta2.numero);
-    printf("digite o saldo da conta: ");
-    scanf("%f", &conta2.saldo);
+    leConta(&conta2, "Digite os dados para a conta2: ");
     exibeConta(conta1, "Conta 1");
     exibeConta(conta2, "Conta 2");
     return 0;
@@ -30,4 +27,13 @@ void exibeConta (struct conta c, char * msg)
     printf("\n%s", msg);
     printf("Numero: %d\n", c.numero);
     printf("Saldo: %.2f\n", c.saldo);
+}
+
+void leConta(struct conta *c, char * msg)
+{
+    printf("\n%s", msg);
+    printf("Digite o numero da conta: ");
+    scanf("%d", c->numero);
+    printf("Digite o saldo da conta: ");
+    scanf("%.2f", c->saldo);
 }
