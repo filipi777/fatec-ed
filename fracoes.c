8d1801741b8dc90cab0e1a33a9f7067424340961
Filipi <you@example.com>
2015-03-06 21:11:22 -0300

    Criação das funções de leitura e escrita das fraçoes

--- fracoes.c
+++ fracoes.c
@@ -6,3 +6,26 @@ typedef struct fracao {
 
 void le_fracao(t_fracao *, char *);
 void mostra_fracao(t_fracao, char *);
+
+int main()
+{
+    t_fracao f1;
+    le_fracao(&f1, "Fracao 1: ");
+    mostra_fracao(f1, "Fracao 1: ");
+    return 0;
+}
+
+le_fracao(t_fracao *f, char * msg)
+{
+    printf("%s", msg);
+    printf("Digite o numerador: ");
+    scanf("%d", &f->num);
+    printf("Digite o denominador: ");
+    scanf("%d", &f->den);
+}
+
+mostra_fracao(t_fracao f, char * msg)
+{
+    printf("%s", msg);
+    printf("%d/%d", f.num, f.den);
+}
