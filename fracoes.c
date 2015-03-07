ac2d4da4e9eff3adfb0feb88f5d432aa5bb15614
Filipi <you@example.com>
2015-03-06 23:24:21 -0300

    Alteracao nas funcoes de fracao

--- fracoes.c
+++ fracoes.c
@@ -9,25 +9,25 @@ void mostra_fracao(t_fracao, char *);
 void inversa1(t_fracao *);
 t_fracao inversa2(t_fracao);
 void multiplica_fracao(t_fracao *, t_fracao *, t_fracao *);
-void divide_fracao(t_fracao *, t_fracao *, t_fracao *);
+t_fracao divide_fracao(t_fracao, t_fracao, t_fracao);
 
 int main()
 {
     t_fracao f1, f2, f3, f4;
-    le_fracao(&f1, "Fracao 1: ");
-    mostra_fracao(f1, "Fracao 1: ");
-    le_fracao(&f2, "Fracao 2: ");
-    mostra_fracao(f2, "Fracao 2: ");
+    le_fracao(&f1, "Fracao 1: \n");
+    mostra_fracao(f1, "\nFracao 1: ");
+    le_fracao(&f2, "\nFracao 2: ");
+    mostra_fracao(f2, "\nFracao 2: ");
     multiplica_fracao(&f1, &f2, &f3);
-    mostra_fracao(f3, "Multiplicacao: ");
-    divide_fracao(&f1, &f2, &f3);
-    mostra_fracao(f3, "Divisao: ");
+    mostra_fracao(f3, "\nMultiplicacao: \n");
+    f4 = divide_fracao(f1, f2, f3);
+    mostra_fracao(f4, "\nDivisao:");
     return 0;
 }
 
 void le_fracao(t_fracao *f, char * msg)
 {
-    printf("%s", msg);
+    printf("%s \n", msg);
     printf("Digite o numerador: ");
     scanf("%d", &f->num);
     printf("Digite o denominador: ");
@@ -36,7 +36,7 @@ void le_fracao(t_fracao *f, char * msg)
 
 void mostra_fracao(t_fracao f, char * msg)
 {
-    printf("%s", msg);
+    printf("%s \n", msg);
     printf("%d/%d", f.num, f.den);
 }
 
@@ -58,14 +58,13 @@ t_fracao inversa2(t_fracao f)
 
 void multiplica_fracao(t_fracao *f, t_fracao *g, t_fracao *r)
 {
-    
-    r->num = f->num * g->den;
-    r->den = f->den * g->num;
+    r->num = f->num * g->num;
+    r->den = f->den * g->den;
 }
 
-void divide_fracao(t_fracao *f, t_fracao *g, t_fracao *r)
+t_fracao divide_fracao(t_fracao f, t_fracao g, t_fracao r)
 {
-    
-    r->num = f->num / g->den;
-    r->den = f->den / g->num;
+    r.num = f.num / g.num;
+    r.den = f.den / g.den;
+    return r;
 }
